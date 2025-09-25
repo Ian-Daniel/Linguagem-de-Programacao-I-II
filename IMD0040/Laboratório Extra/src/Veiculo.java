466e4a90dbdec0743737b1e81d659fba663d6505
Ian Marques <iandaniel.vm@gmail.com>
2025-09-25 20:25:24 -0300

    Iniciar atividade de laboratório extra
    Escritas as primeiras linhas de código e criados os arquivos iniciais do projeto. Ainda em estágio inicial e abstrato, mas já estabelecendo a base do desenvolvimento.

---
+++ IMD0040/Laboratório Extra/src/Veiculo.java
@@ -0,0 +1,19 @@
+
+public abstract class Veiculo implements Fretavel {
+
+    private String placa;
+    private int ano;
+
+    public Veiculo(String placa, int ano) {
+        this.placa = placa;
+        this.ano = ano;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+}
