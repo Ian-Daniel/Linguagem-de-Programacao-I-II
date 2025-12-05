e665d9c66af282a197635a4d0da5d8712d3ace9c
nandosmpa <72308702+nandosmpa@users.noreply.github.com>
2025-12-05 16:21:25 -0300

    Fix missing newline at end of JogadorController.java

--- IMD0040/Projeto - 2ª Unidade/frontend/src/main/java/com/ufrn/elenco/ui/controllers/JogadorController.java
+++ IMD0040/Projeto - 2ª Unidade/frontend/src/main/java/com/ufrn/elenco/ui/controllers/JogadorController.java
@@ -4,10 +4,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class JogadorController {
+
     @FXML
     private Label lblJogadorNome;
+
     @FXML
     private Label lblJogadorIdade;
+
     @FXML
     private Label lblJogadorPosicao;
 
