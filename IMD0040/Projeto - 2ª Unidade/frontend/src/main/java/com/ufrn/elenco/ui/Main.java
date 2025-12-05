46e93958b4825fbde2d5177f1d3c2d9216953937
nandosmpa <72308702+nandosmpa@users.noreply.github.com>
2025-12-05 16:20:10 -0300

    Update Main.java

--- IMD0040/Projeto - 2ª Unidade/frontend/src/main/java/com/ufrn/elenco/ui/Main.java
+++ IMD0040/Projeto - 2ª Unidade/frontend/src/main/java/com/ufrn/elenco/ui/Main.java
@@ -11,9 +11,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                getClass().getResource("/com/ufrn/elenco/ui/resources/fxml/MainView.fxml")
+        );
         StackPane root = fxmlLoader.load();
-        
+
         Scene scene = new Scene(root, 800, 600);
         stage.setTitle("Elenco UFRN");
         stage.setScene(scene);
