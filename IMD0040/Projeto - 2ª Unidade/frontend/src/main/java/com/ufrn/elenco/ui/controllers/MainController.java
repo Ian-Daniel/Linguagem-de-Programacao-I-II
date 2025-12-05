c79657a372dd3be069adbd89128758a1014e6d6e
nandosmpa <72308702+nandosmpa@users.noreply.github.com>
2025-12-05 16:20:48 -0300

    Implement navigation to JogadorListView.fxml

--- IMD0040/Projeto - 2ª Unidade/frontend/src/main/java/com/ufrn/elenco/ui/controllers/MainController.java
+++ IMD0040/Projeto - 2ª Unidade/frontend/src/main/java/com/ufrn/elenco/ui/controllers/MainController.java
@@ -1,16 +1,35 @@
 package com.ufrn.elenco.ui.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
+
     @FXML
     private Button btnShowJogadores;
 
     @FXML
     public void initialize() {
         btnShowJogadores.setOnAction(e -> {
-            // Ação de navegar para a tela de jogadores
+            try {
+                FXMLLoader loader = new FXMLLoader(
+                        getClass().getResource("/com/ufrn/elenco/ui/resources/fxml/JogadorListView.fxml")
+                );
+                Parent root = loader.load();
+
+                Stage stage = (Stage) btnShowJogadores.getScene().getWindow();
+                stage.setScene(new Scene(root, 800, 600));
+                stage.setTitle("Lista de Jogadores - Elenco UFRN");
+                stage.show();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         });
     }
 }
